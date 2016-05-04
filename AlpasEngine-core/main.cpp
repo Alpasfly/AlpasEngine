50139c1b3e27b5f773111b13cd21a1dea5f0a953
alpasfly <alpasfly@gmail.com>
2016-05-04 23:49:18 +0200

    First commit LOL

---
+++ AlpasEngine-core/main.cpp
@@ -0,0 +1,86 @@
+#include "src/graphics/window.h"
+#include "src/maths/maths.h"
+#include "src\graphics\shader.h"
+
+#include "src\graphics\buffers\buffer.h"
+#include "src\graphics\buffers\indexbuffer.h"
+#include "src\graphics\buffers\vertexarray.h"
+
+#include "src/graphics/renderer2d.h"
+#include "src/graphics/renderable2d.h"
+#include "src/graphics/simple2drenderer.h"
+#include "src/graphics/batchrenderer2d.h"
+#include "src\utils\timer.h"
+
+#include "src/graphics/layers/tilelayer.h"
+
+#include "src/graphics/sprite.h"
+
+#include <time.h>
+
+int main() 
+{
+	using namespace alpas;
+	using namespace graphics;
+	using namespace maths;
+	
+	Window window("Test", 960, 540);
+	//glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	mat4 ortho = mat4::orthographic(0.0f, 16.0f, 0.0f, 9.0f, -1.0f, 1.0f);
+
+	Shader* s = new Shader("src/shaders/basic.vert", "src/shaders/basic.frag");
+	Shader* s2 = new Shader("src/shaders/basic.vert", "src/shaders/basic.frag");
+	Shader& shader = *s;
+	Shader& shader2 = *s2;
+
+	shader.enable();
+	shader2.enable();
+
+	shader.setUniform2f("light_pos", vec2(4.0f, 1.5f));
+	shader2.setUniform2f("light_pos", vec2(4.0f, 1.5f));
+
+	TileLayer layer(&shader);
+
+	for (float y = -9.0f; y < 9.0f; y += 0.1f)
+	{
+		for (float x = -16.0f; x < 16.0f; x += 0.1f)
+		{
+			layer.add(new Sprite(x, y, 0.09f, 0.09f, maths::vec4(rand() % 1000 / 1000.0f, 0, 1, 1)));
+		}
+	}
+
+	TileLayer layer2(&shader2);
+	layer2.add(new Sprite(-2, -2, 4, 4, maths::vec4(1, 0, 1, 1)));
+
+	Timer time;
+	float timer = 0;
+	unsigned int frames = 0;
+
+	while (!window.closed())
+	{
+		window.clear();
+
+		double x, y;
+		window.getMousePosition(x, y);
+		shader.enable();
+		shader.setUniform2f("light_pos", vec2(-8, -3));
+		shader2.enable();
+		shader2.setUniform2f("light_pos", vec2((float)(x * 32.0f / 960.0f - 16.0f), (float)(9.0f - y * 18.0f / 540.0f)));
+
+		layer.render();
+		layer2.render();
+
+		window.update();
+		frames++;
+
+		if (time.elapsed() - timer > 1.0f)
+		{
+			timer += 1.0f;
+			printf("%d fps\n", frames);
+			frames = 0;
+		}
+	}
+	
+	return 0;
+}
---
+++ AlpasEngine-core/src/graphics/simple2drenderer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <deque>
+#include "static_sprite.h"
+#include "renderer2d.h"
+
+namespace alpas {
+	namespace graphics {
+
+		class Simple2DRenderer : public Renderer2D
+		{
+		private:
+			std::deque<const StaticSprite*> m_RenderQueue;
+		public:
+			void submit(const Renderable2D* renderable) override;
+			void flush() override;
+		};
+	}
+}
